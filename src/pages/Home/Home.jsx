868ff618061bf8fe253020726b07bf54bce1d4a5
Ananthu Shaji <anandhushajiareeckal2002@gmail.com>
2025-11-22 23:33:10 +0530

    Refactor code structure for improved readability and maintainability

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Header from '../../components/layout/Header'
 import Footer from '../../components/layout/Footer'
 import Preloader from '../../components/layout/Preloader'
+import Main from '../../components/home/Main'
+
+
 
 const Home = () => {
   return (
     <>
       <Preloader />
       <Header />
+      <Main />
       <Footer />
     </>
   )
